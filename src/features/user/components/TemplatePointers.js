2614173e262def9aaf21e4a8b004750fac4ccea4
Vatche Chamlian <vatche.chamlian@gmail.com>
2024-11-21 15:13:20 -0500

    changed some content nothing major

--- src/features/user/components/TemplatePointers.js
+++ src/features/user/components/TemplatePointers.js
@@ -1,12 +1,12 @@
 function TemplatePointers(){
     return(
         <>
-         <h1 className="text-2xl mt-8 font-bold">Admin Dashboard Starter Kit</h1>
+         <h1 className="text-2xl mt-8 font-bold">Welcome to myParently</h1>
           <p className="py-2 mt-4">✓ <span className="font-semibold">Light/dark</span> mode toggle</p>
-          <p className="py-2 ">✓ <span className="font-semibold">Redux toolkit</span> and other utility libraries configured</p>
-          <p className="py-2">✓ <span className="font-semibold">Calendar, Modal, Sidebar </span> components</p>
-          <p className="py-2  ">✓ User-friendly <span className="font-semibold">documentation</span></p>
-          <p className="py-2  mb-4">✓ <span className="font-semibold">Daisy UI</span> components, <span className="font-semibold">Tailwind CSS</span> support</p>
+          <p className="py-2 ">✓ <span className="font-semibold">AIdvocate</span> chat with our AI agent for IEP support</p>
+          <p className="py-2">✓ <span className="font-semibold">Add children and their IEPs </span> in your workspace</p>
+          <p className="py-2  ">✓ Add another <span className="font-semibold">parent or guardian</span></p>
+          <p className="py-2  mb-4">✓ <span className="font-semibold">Remember this is in BETA</span></p>
         </>
     )
 }
